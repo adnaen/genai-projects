87bea0178af05a508ab1d2a676cb76b5b0f5970b
Adnan <adnanmuhammedadnancp@gmail.com>
2025-06-27 03:30:15 +0530

    update ui

--- search_with_documents/ui/script.js
+++ search_with_documents/ui/script.js
@@ -1,6 +1,6 @@
 const fileUploadForm = document.getElementById("fileUploadForm");
 const fileInput = document.getElementById("dropzone-file");
-
+const fileResultField = document.getElementById("fileDataSection");
 const API_ROUTE = "http://localhost:8000/api/v1";
 
 fileUploadForm.addEventListener("submit", (event) => {
@@ -15,14 +15,51 @@ fileUploadForm.addEventListener("submit", (event) => {
     body: formData,
   })
     .then((response) => {
-      if (!response.status == "201") {
-        console.log("its fine");
+      console.log(response.status);
+      if (!response.ok) {
+        throw new Error(
+          `something went wrong with status code : ${response.status}`,
+        );
       } else {
-        console.log("something wrong");
+        return response.json();
       }
-      return response.json();
     })
     .then((data) => {
-      console.log(data);
+      addNewFileInfo(data.name, data.created_at);
+    })
+    .catch((err) => {
+      alert(`something went wrong : ${err}`);
     });
 });
+
+const fetchFileInfo = async () => {
+  const response = await fetch(`${API_ROUTE}/file`, {
+    method: "GET",
+  });
+
+  if (response.status == 200) {
+    const data = await response.json();
+    data.forEach((each) => {
+      addNewFileInfo(each.name, each.created_at);
+    });
+  }
+};
+
+const info = fetchFileInfo();
+
+const addNewFileInfo = (filename, date) => {
+  const divHere = document.createElement("div");
+
+  const h3Here = document.createElement("h3");
+  h3Here.className = "text-lg font-bold";
+  h3Here.innerText = filename;
+
+  const spanHere = document.createElement("span");
+  spanHere.className = "text-md text-slate-400";
+  spanHere.innerText = date;
+
+  divHere.appendChild(h3Here);
+  divHere.appendChild(spanHere);
+
+  fileResultField.appendChild(divHere);
+};
